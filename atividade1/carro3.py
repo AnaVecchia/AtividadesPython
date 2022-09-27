c424319135bdf1311c35e9c5d5f17dafcfff9388
Ana Júlia Della Vecchia <114075589+AnaVecchia@users.noreply.github.com>
2022-09-27 10:57:01 -0300

    Create carro3.py

---
+++ atividade1/carro3.py
@@ -0,0 +1,11 @@
+from carro2 import Carro
+
+carroAna1=  Carro("Volks","Santana","Prata","Gasolina")
+carroAna2 = Carro("lamborghini","SVJ","Amarela","Gasolina")
+
+carroAna1.ligar()
+carroAna1.acelerar()
+carroAna1.desligar()
+
+print(vars(carroAna1))
+print(vars(carroAna2))
