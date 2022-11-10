193e8dc6cb827d57c251d790ead5022a6679f0c2
Ana Júlia Della Vecchia <114075589+AnaVecchia@users.noreply.github.com>
2022-11-10 16:15:58 -0300

    Delete atividade 1 directory

--- atividade 1/caro3 (2).py
+++
@@ -1,7 +0,0 @@
-from carro4 import Carro, Proprietario
-
-Ana = Proprietario("Ana Julia", "Della Vecchia", "Santana")
-
-carroAna1 = Carro("Volks","Santana","Prata", "Gasolina", Ana)
-
-print(carroAna1.proprietario.sobrenome)
