1c21d76b39a70779533a9b58785ec7bf25a0d517
Ana Júlia Della Vecchia <114075589+AnaVecchia@users.noreply.github.com>
2022-11-10 16:24:32 -0300

    Create caro3 (2).py

---
+++ atividade1/caro3 (2).py
@@ -0,0 +1,7 @@
+from carro4 import Carro, Proprietario
+
+Ana = Proprietario("Ana Julia", "Della Vecchia", "Santana")
+
+carroAna1 = Carro("Volks","Santana","Prata", "Gasolina", Ana)
+
+print(carroAna1.proprietario.sobrenome)
